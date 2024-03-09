9e5b8c247297a0f8570b9857a1b7765b7c2a9357
dadoming <davidmdg98@hotmail.com>
2024-03-09 11:36:42 +0000

    ready for now

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sort People into teams
+
+- Go into main.py and set the minimum and maximum team sizes.
+- Create a file named names.txt and put the names of the people you want to sort into teams in it, one name per line.
+- Run main.py and it will create a file named teams.csv with the teams in it.
+
+## To Run:
+```bash
+git clone https://github.com/dadoming/SortPeopleIntoTeams.git
+```
+```bash
+cd SortPeopleIntoTeams
+```
+```bash
+python3 main.py
+```
+
+### To add:
+- Add a way if there are 6 people, for example, make it sort into 2 teams of 3 instead of 3 teams of 2.
+- Add a way to insert remaining people into teams with less people.
---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+
+MINIMUM_TEAM_SIZE = 2
+MAXIMUM_TEAM_SIZE = 4
+
+if not MINIMUM_TEAM_SIZE or not MAXIMUM_TEAM_SIZE or MINIMUM_TEAM_SIZE > MAXIMUM_TEAM_SIZE:
+    print("Wrong team size specification")
+    exit()
+
+names = []
+with open('names.txt') as f:
+    names = f.read().splitlines()
+if not names:
+    print("No names specified")
+    exit()
+random.shuffle(names)
+
+total_names = len(names)
+if MINIMUM_TEAM_SIZE > len(names):
+    print("Team sizes are too large or there are too few names")
+    exit()
+
+print(f"Total:\t{total_names} names")
+print(f"Sizes:\t{MINIMUM_TEAM_SIZE}-{MAXIMUM_TEAM_SIZE} people")
+
+def distribute_teams(names):
+    team_sizes = list(range(MAXIMUM_TEAM_SIZE, MINIMUM_TEAM_SIZE - 1, -1))
+    teams = {size: [] for size in team_sizes}
+    for size in team_sizes:
+        while names and len(names) >= size:
+            team = [names.pop() for _ in range(size)]
+            teams[size].append(team)
+    for name in names:
+        smallest_team_size = min(teams.keys())
+        teams[smallest_team_size].append([name])
+    return teams
+
+teams = distribute_teams(names)
+
+if names:
+    print(f"\nTeams have been generated but there are players left.\nRemaining names: {names}")
+else:
+    print(f"\nAll names have been successfully distributed")
+
+import csv
+with open('teams.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Team', 'Team Size', '1', '2', '3', '4'])
+    writer.writerows([f'Team {i}', size, *team] for size, team_list in teams.items() for i, team in enumerate(team_list, start=1))
+    if names:
+        writer.writerow(['Remaining names', '', *names])
